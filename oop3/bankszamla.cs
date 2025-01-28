859f0170c5ff22fd4d2ac33ee6d311b877ad489f
dodi1772 <zsigmond.dorian01@gmail.com>
2025-01-28 08:37:40 +0100

    Elso comm

--- oop3/bankszamla.cs
+++ oop3/bankszamla.cs
@@ -8,6 +8,47 @@ namespace oop3
 {
 	internal class bankszamla
 	{
+		//1. statikus getter setter
+		private static int kovetkezoSzamlaSzam = 1000;
+		public string SzamlaSzam {  get; private set; }
+		public string TulNev {  get; private set; }
+		public int Egyenleg {  get; private set; }
 
+		//konstruktorok
+		//init kezdeti érték
+		//public [osztaly neve]
+		public bankszamla(string tulnev, int kezdoegyenleg)
+		{
+			SzamlaSzam = $"ACC{kovetkezoSzamlaSzam++}";
+			TulNev = tulnev;
+			Egyenleg= kezdoegyenleg;
+		}
+
+		//befizet method
+		public void befizet(int osszeg)
+		{
+            if (osszeg>0)
+            {
+				Egyenleg += osszeg;
+                Console.WriteLine($"Az összeg sikeresen befizetve a(z) {SzamlaSzam} számlára.");
+
+            }
+            else
+            {
+                Console.WriteLine("Hibás összeg");
+            }
+        }
+		public void kifizet(int osszeg)
+		{
+            if (osszeg>0 && Egyenleg>=osszeg)
+            {
+				Egyenleg -= osszeg;
+                Console.WriteLine($"Az összeg kifizetve a(z) {SzamlaSzam} számláról.");
+            }
+            else
+            {
+                Console.WriteLine("Az összeg nem lehet több mint a rendelkezésre álló egyenleg, vagy negatív.");
+            }
+        }
 	}
 }
