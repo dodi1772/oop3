e0c757e768adb9d73f7b73b1c3b20b2b8ae451fa
Dorian <zsigmond.dorian01@gmail.com>
2025-01-30 21:30:25 +0100

    comm5

---
+++ oop3/Tranzakcio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_gyak
+{
+    internal class Tranzakcio
+    {
+        public string Tipus { get; }
+        public int Osszeg { get; }
+        public DateTime Datum { get; }
+
+        public Tranzakcio(string tipus, int osszeg, DateTime datum)
+        {
+            Tipus = tipus;
+            Osszeg = osszeg;
+            Datum = datum;
+        }
+
+        public override string ToString()
+        {
+            return $"{Datum}: {Tipus} - {Osszeg} Ft";
+        }
+
+    }
+}
